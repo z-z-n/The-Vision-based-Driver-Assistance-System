6d41eab31b02ae3f30756eac51d8917d2c274f28
z-z-n <zhang.chihning@gmail.com>
2023-04-20 18:49:30 -0700

    kitti训练CA模型15轮，修改实验结果文件夹

--- detect.py
+++ detect.py
@@ -239,9 +239,8 @@ def parse_opt():
     # parser.add_argument('--source', type=str, default=ROOT / 'data/video', help='source')
     parser.add_argument('--data', type=str, default=ROOT / 'data/coco128.yaml', help='(optional) dataset.yaml path')
     parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
-    # 置信度阈值默认0.25
-    parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold') # 置信度阈值默认0.25
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')   # 0.45
     parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--view-img', action='store_true', help='show results')
